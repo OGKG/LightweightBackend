a690118ad436ef18333ac58045c4b93e49ba774d
Chmele <killerbbb123@gmail.com>
2022-09-01 16:38:28 +0300

    Extend models with module

--- core/models.py
+++ core/models.py
@@ -11,7 +11,13 @@ class Task(models.Model):
     type = models.TextField(choices=list(zip(mapping.keys(), mapping.keys())))
     users = models.ManyToManyField(get_user_model(), 'tasks', through="Mark")
 
+
 class Mark(models.Model):
     user = models.ForeignKey(get_user_model(), models.CASCADE, 'marks')
     task = models.ForeignKey(Task, models.CASCADE, 'marks')
     mark = models.FloatField()
+
+
+class Module(models.Model):
+    tasks = models.ManyToManyField(Task, 'modules')
+
